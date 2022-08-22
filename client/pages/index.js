a8263d125bedea1d443c0b1633c632960a7b7cb0
Shafin The Developer <sahariar350@gmail.com>
2022-08-22 19:20:23 +0600

    Header done

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,14 +3,17 @@ import Testimonials from '../components/Home/Testimonials/Testimonials';
 import WorkProcess from '../components/Home/WorkProcess/WorkProcess';
 import Footer from '../components/shared/Footer/Footer';
 import Navbar from './../components/shared/Navbar/Navbar';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <HeroSec />
+      <div className={styles.headerWrapper}>
+        <Navbar />
+        <HeroSec />
+      </div>
       <WorkProcess />
-      <Testimonials/>
+      <Testimonials />
       <Footer />
     </div>
   )
