c2894444e7ae74696bf4a33cef36b8182d626a43
SAMIUR <samiurkhan94@gmail.com>
2022-08-23 21:42:48 +0600

    pagination component logic built

--- client/components/Blog/Pagination/Nav.js
+++ client/components/Blog/Pagination/Nav.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import styles from './Nav.module.css'
+import React from "react";
+import styles from "./Nav.module.css";
 
 export default function Nav() {
   return (
     <div className={styles.button_container}>
-        <div className={styles.}>
-            
-        </div>
+      <nav aria-label="Page navigation example">
+        <ul className={`pagination ${styles.studiePagination}`}>
+          <li className="page-item">
+            <a
+              className={`page-link ${styles.PaginationButton}`}
+              id={styles.PaginationButton}
+              href="#"
+            >
+              {" "}
+              <i className="ri-arrow-left-line" id={styles.iconLeft}></i>{" "}
+              Previous
+            </a>
+          </li>
+          <li className="page-item">
+            <a className={`page-link ${styles.PaginationItem}`} href="#">
+              1
+            </a>
+          </li>
+          <li className="page-item">
+            <a className={`page-link ${styles.PaginationItem}`} href="#">
+              2
+            </a>
+          </li>
+          <li className="page-item">
+            <a className={`page-link ${styles.PaginationItem}`} href="#">
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className={`page-link ${styles.PaginationButton}`} href="#">
+              Next <i className="ri-arrow-right-line" id={styles.iconRight}></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
