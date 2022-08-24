cbdeeea9ee7c60dbf622c764d5b20f5d98e18ade
Shafin The Developer <sahariar350@gmail.com>
2022-08-24 21:21:39 +0600

    Page link and nav link added

--- client/components/shared/Navbar/Navbar.js
+++ client/components/shared/Navbar/Navbar.js
@@ -1,18 +1,21 @@
 import Image from 'next/image';
 import React from 'react';
 import styles from './Navbar.module.css';
+import Link from 'next/link';
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
             <div className="container">
-                <a className="navbar-brand" href="#">
-                    <Image
-                        src={'/images/Brand_Logo.svg'}
-                        width={200}
-                        height={50}
-                    />
-                </a>
+                <Link href="/">
+                    <a className="navbar-brand" href="#">
+                        <Image
+                            src={'/images/Brand_Logo.svg'}
+                            width={200}
+                            height={50}
+                        />
+                    </a>
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -21,18 +24,26 @@ function Navbar() {
                         <li className="nav-item">
                             <a className={`nav-link active me-5 ${styles.navItem}`} aria-current="page" href="#">Service</a>
                         </li>
-                        <li className="nav-item">
-                            <a className={`nav-link active me-5 ${styles.navItem}`} aria-current="page" href="#">Our Work</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className={`nav-link active me-5 ${styles.navItem}`} aria-current="page" href="#">Articles</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className={`nav-link active me-5 ${styles.navItem}`} aria-current="page" href="#">About Us</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className={`nav-link active ${styles.navBtn}`} aria-current="page" href="#">Contact Us <i className="ri-arrow-right-line"></i></a>
-                        </li>
+                        <Link href='/case-studies'>
+                            <li className="nav-item">
+                                <a className={`nav-link active me-5 ${styles.navItem}`} aria-current="page" href="#">Our Work</a>
+                            </li>
+                        </Link>
+                        <Link href='/blog'>
+                            <li className="nav-item">
+                                <a className={`nav-link active me-5 ${styles.navItem}`} aria-current="page" href="#">Articles</a>
+                            </li>
+                        </Link>
+                        <Link href='/about'>
+                            <li className="nav-item">
+                                <a className={`nav-link active me-5 ${styles.navItem}`} aria-current="page" href="#">About Us</a>
+                            </li>
+                        </Link>
+                        <Link href='/contact'>
+                            <li className="nav-item">
+                                <a className={`nav-link active ${styles.navBtn}`} aria-current="page" href="#">Contact Us <i className="ri-arrow-right-line"></i></a>
+                            </li>
+                        </Link>
                     </ul>
                 </div>
             </div>
