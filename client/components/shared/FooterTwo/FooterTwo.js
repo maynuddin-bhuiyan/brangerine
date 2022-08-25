0dde0f129ad856222167dc1f41c8c3d1dbd53eed
Shafin The Developer <sahariar350@gmail.com>
2022-08-26 01:25:39 +0600

    Done

--- client/components/shared/FooterTwo/FooterTwo.js
+++ client/components/shared/FooterTwo/FooterTwo.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import styles from "./FooterTwo.module.css";
 
@@ -5,7 +6,7 @@ const FooterTwo = () => {
     return (
         <div className='container'>
             <div className={styles.footerTwo}>
-                <h1>2021 Relume. All right reserved.</h1>
+                <h1>2021 Relume. All right reserved. || Developed by <Link href="https://apexdv.com/"><a target="_blank" rel="noopener noreferrer">ApexDv</a></Link></h1>
                 <div>
                     <a href="#">Privacy Policy</a>
                     <a href="#">Terms of Service</a>
