78ba8988c504541e452f77631f7b394bb5a88043
Shafin The Developer <sahariar350@gmail.com>
2022-08-25 17:25:49 +0600

    Fixed blog slider

--- client/components/Blog/Carousel/Carousel.js
+++ client/components/Blog/Carousel/Carousel.js
@@ -1,15 +1,18 @@
-import React, { createRef,useEffect,useState } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import styles from "./Carousel.module.css";
 import Photo from "./Photo";
 import Slider from "react-slick";
-import {photos} from './photoData'
+import { photos } from './photoData'
 export default function Carousel() {
-  const [width,setWidth] = useState(0)
+  const [width, setWidth] = useState(0)
   useEffect(() => {
-    if(typeof window !== undefined){
+    if (typeof window !== undefined) {
       setWidth(window.innerWidth)
     }
-  },[])
+  }, []);
+  const [sliderIndex, setSliderIndex] = useState({
+    next: 2
+  });
   const PhotoSlider = createRef();
   const settings = {
     dots: width <= 768 ? true : false,
@@ -18,6 +21,12 @@ export default function Carousel() {
     speed: 500,
     slidesToShow: 2.35,
     slidesToScroll: 1,
+    beforeChange: (current, next) => {
+      setSliderIndex({ current, next: next + 2 });
+    },
+    // afterChange: (current, next) => {
+    //   setSliderIndex({ current: current + 1, next });
+    // },
     responsive: [
       {
         breakpoint: 1024,
@@ -48,52 +57,29 @@ export default function Carousel() {
   console.log(settings)
   const gotoNext = () => {
     PhotoSlider.current.slickNext();
+    console.log(sliderIndex, "sliderIndex");
   };
 
   const gotoPrev = () => {
     PhotoSlider.current.slickPrev();
+    console.log(sliderIndex, "sliderIndex");
   };
-  if(width !== 0){
-    if(width >= 768){
-      return (
-        <div className={styles.carousel_container}>
-          <Slider {...settings} ref={PhotoSlider}>
-            {
-                photos.map(photo => (
-                    <Photo id={photo.id} photo={photo}/>
-                ))
-            }
-          </Slider>
-          <div className={styles.slider_buttons}>
-            <button onClick={() => gotoPrev()} className={styles.slider_button_left}>
-            <i className="fa-solid fa-arrow-left"></i>
-            </button>
-            <button onClick={() => gotoNext()} className={styles.slider_button_right}>
-            <i className="fa-solid fa-arrow-right"></i>
-            </button>
-          </div>
-        </div>
-      );
-    }else{
-      return(
-        <div className={styles.carousel_container}>
-          <Slider {...settings} ref={PhotoSlider}>
-            {
-                photos.map(photo => (
-                    <Photo id={photo.id} index={photo.id} photo={photo}/>
-                ))
-            }
-          </Slider>
-          {/* <div className={styles.slider_buttons}>
-            <button onClick={() => gotoPrev()} className={styles.slider_button_left}>
-            <i className="fa-solid fa-arrow-left"></i>
-            </button>
-            <button onClick={() => gotoNext()} className={styles.slider_button_right}>
-            <i className="fa-solid fa-arrow-right"></i>
-            </button>
-          </div> */}
-        </div>
-      )
-    }
-  }
+  return (
+    <div className={styles.carousel_container}>
+      <Slider {...settings} ref={PhotoSlider}>
+        {
+          photos.map((photo, index) => (
+            <Photo
+              id={photo.id}
+              index={index}
+              photo={photo}
+              gotoPrev={gotoPrev}
+              gotoNext={gotoNext}
+              sliderIndex={sliderIndex}
+            />
+          ))
+        }
+      </Slider>
+    </div>
+  )
 }
